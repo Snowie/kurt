61f44ae951851c9a099a17131cce5116407dd540
Samantha Doran <samanthadoran3@gmail.com>
2015-08-05 00:25:10 -0400

    Add scrolling views, doesn't handle recursive scrolls

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -5,9 +5,57 @@ public class UIManager : MonoBehaviour {
 	public UnityEngine.UI.Button button;
 	public UnityEngine.UI.InputField inputField;
 	public UnityEngine.UI.Text textField;
+	public GameObject scrollView;
 
 
 	// Use this for initialization
+	void panelLogic(Transform panel,JSONArray components) {
+		Transform cParent = panel.transform;
+		foreach (JSONNode uiObj in components) {
+			JSONNode location = uiObj ["location"];
+			JSONNode size = uiObj ["size"];
+			
+			//Determine the type of ui object to render
+			switch (uiObj ["type"]) {
+			case "KTButton":
+				UnityEngine.UI.Button buttonClone = Instantiate (button);
+				//Update the rect transform as per data
+				var buttonRT = buttonClone.GetComponent<RectTransform> ();
+				buttonRT.anchoredPosition = new Vector2 (location ["x"].AsFloat + panel.position.x, location ["y"].AsFloat + panel.position.y);
+				buttonRT.sizeDelta = new Vector2 (size ["width"].AsFloat, size ["height"].AsFloat);
+				
+				//Set the UI's parent to the canvas	
+				buttonClone.transform.SetParent (cParent);
+				break;
+				
+			case "KTInputField":
+				UnityEngine.UI.InputField inputFieldClone = Instantiate (inputField);
+				//Update the rect transform as per data
+				var inputFieldRT = inputFieldClone.GetComponent<RectTransform> ();
+				inputFieldRT.anchoredPosition = new Vector2 (location ["x"].AsFloat + panel.position.x, location ["y"].AsFloat + panel.position.y);
+				inputFieldRT.sizeDelta = new Vector2 (size ["width"].AsFloat, size ["height"].AsFloat);
+				
+				//Set the UI's parent to the canvas	
+				inputFieldClone.transform.SetParent (cParent);
+				break;
+				
+			case "KTLabel":
+				UnityEngine.UI.Text textFieldClone = Instantiate (textField);
+				
+				textFieldClone.text = uiObj ["id"].ToString ();
+				
+				//Update the rect transform as per data
+				var textFieldRT = textFieldClone.GetComponent<RectTransform> ();
+				textFieldRT.anchoredPosition = new Vector2 (location ["x"].AsFloat + panel.position.x, location ["y"].AsFloat + panel.position.y);
+				textFieldRT.sizeDelta = new Vector2 (size ["width"].AsFloat, size ["height"].AsFloat);
+				
+				//Set the UI's parent to the canvas	
+				textFieldClone.transform.SetParent (cParent);
+				break;
+			}
+		}
+	}
+
 	void Start () {
 		string url = "http://snowie.github.io/kurt/";
 		WWW uiResponse = new WWW (url);
@@ -64,6 +112,22 @@ public class UIManager : MonoBehaviour {
 				
 					//Set the UI's parent to the canvas	
 					textFieldClone.transform.SetParent(this.transform.parent);
+				break;
+				case "KTScrollView":
+					GameObject scrollViewClone = Instantiate(scrollView);
+					
+					//Update rect transform of scroll view
+					var scrollViewRT = scrollViewClone.GetComponent<RectTransform>();
+					scrollViewRT.anchoredPosition = new Vector2(location["x"].AsFloat, location["y"].AsFloat);
+					scrollViewRT.sizeDelta = new Vector2(size["width"].AsFloat, size["height"].AsFloat);
+					
+					scrollViewClone.transform.SetParent(this.transform.parent);
+					
+					//Update rect transform of panel
+					RectTransform panel = scrollViewClone.transform.GetChild(0).GetComponent<RectTransform>();
+					panel.sizeDelta = new Vector2(uiObj["panelsize"]["width"].AsFloat, uiObj["panelsize"]["height"].AsFloat);
+					
+					panelLogic(scrollViewClone.transform.GetChild(0).GetComponent<RectTransform>(), (JSONArray)uiObj["components"]);
 				break;
 			}
 		}
